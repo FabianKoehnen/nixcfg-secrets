e2272a077f4a7cb4bfc6a87d0f86e458397c607f
fabianKoehnen <fk@ambimax.de>
2023-09-19 00:26:42 +0200

    symlink

--- desktop/default.nix
+++ desktop/default.nix
@@ -2,4 +2,8 @@
 
 {
   users.users.fabian.hashedPassword = "$y$j9T$LWe4pH/0thxFzM6AaiZEQ0$o8NSwRSOYeSkvM5AQI0LUv/X19P4hTkiReSr24dUbC/";
+
+  systemd.tmpfiles.rules = [
+    "L /root/.ssh - - - - /home/fabian/.ssh"
+  ];
 }
