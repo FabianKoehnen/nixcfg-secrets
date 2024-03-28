2c435a4a2fdea9a247cf49973f74882385698c5f
fabianKoehnen <42027473+FabianKoehnen@users.noreply.github.com>
2024-03-28 21:10:01 +0100

    init laptop

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   outputs = _: {
     nixosModules.desktop = import ./hosts/desktop/default.nix;
+    nixosModules.laptop = import ./hosts/laptop/default.nix;
   };
 }
---
+++ hosts/laptop/default.nix
@@ -0,0 +1,11 @@
+{...}: {
+  imports = [
+    ../../modules/yubikey
+  ];
+
+  users.users.fabian.hashedPassword = "$y$j9T$LWe4pH/0thxFzM6AaiZEQ0$o8NSwRSOYeSkvM5AQI0LUv/X19P4hTkiReSr24dUbC/";
+
+  systemd.tmpfiles.rules = [
+    "L /root/.ssh - - - - /home/fabian/.ssh"
+  ];
+}
