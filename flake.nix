f643964c88005e6ab2abb3b2b75408e84c87465d
fabianKoehnen <42027473+FabianKoehnen@users.noreply.github.com>
2024-06-24 13:56:15 +0200

    prepared for tuxedosystem

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   outputs = _: {
     nixosModules.desktop = import ./hosts/desktop/default.nix;
+    nixosModules.tuxSiriusGen2 = import ./hosts/work/tuxSiriusGen2/default.nix;
   };
 }
---
+++ hosts/work/tuxSiriusGen2/default.nix
@@ -0,0 +1,11 @@
+{...}: {
+  imports = [
+    ../../modules/yubikey
+  ];
+
+  users.users.fabian.hashedPassword = "$y$j9T$ZRSrGyql6kwEpH4RXtD8h.$pJAQKVbbvmuPd25VqUCK867M0cUG.dWoS2fDtUH/S33";
+
+  systemd.tmpfiles.rules = [
+    "L /root/.ssh - - - - /home/fabian/.ssh"
+  ];
+}
